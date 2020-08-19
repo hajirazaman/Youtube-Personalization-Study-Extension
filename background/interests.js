8c66ef941784cdc175f347e8aee751a1afb86a5b
Hajira Zaman <hajirazam@gmail.com>
2020-08-19 15:14:57 +0500

    made interest specific age and gender optional since not all users have this

--- background/interests.js
+++ background/interests.js
@@ -14,14 +14,20 @@ function getInterestData() {
 	Http.onreadystatechange= async function (){
     	if(this.readyState == HTTP_READYSTATE_DONE && this.status == HTTP_STATUS_OK){
     		var data = Http.responseText;
-			//console.log(data)	
+			// console.log(data);
 			try {
-				var age = data.match(/aria\-label=\"Age:(.*?)"/g)[0];
-				age = age.substring(16, age.length-1);
-				var gender = data.match(/aria\-label=\"Gender:(.*?)"/g)[0];
-				gender = gender.substring(20, 26);
+				var age=gender=null;
+				try {
+					age = data.match(/aria\-label=\"Age:(.*?)"/g)[0];
+					age = age.substring(17, age.length-1);
+					gender = data.match(/aria\-label=\"Gender:(.*?)"/g)[0];
+					gender = gender.substring(20, 26);
+				} catch(e) {
+					console.log("Exceptiong in collecting Interests Age and Gender "+ e);
+				}
+				console.log("Age: "+age+" Gender: "+gender);
 				//console.log(data.match(/\[\[\[\[(.*\n)*\}\}\)/g), "y");	
-				var regex_interests = /\[null,"[0-9]+","([^"]*)/g;
+				var regex_interests = /\[null,"[0-9]+","([^"]*)"/g;
 				interests = getAllRegexMatches(regex_interests, data)
 				// console.log(interests)
 				person.userInterests.push({
